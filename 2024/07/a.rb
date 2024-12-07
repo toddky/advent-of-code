00e3907ad2ded1688b2030772a0af95508ecc22a
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-12-06 23:17:02 -0600

    2024/07: Solved!

---
+++ 2024/07/a.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+# ==============================================================================
+# SETUP
+# ==============================================================================
+require_relative '../../todd'
+
+inputs = Hash.new
+inputs[0] = 'input.txt'.read
+inputs[2] = <<-EOF
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+EOF
+
+input = inputs[0]
+#input = inputs[2]
+NUMS  = input.lines.map(&:numbers)
+
+# ==============================================================================
+# CODE
+# ==============================================================================
+rows = NUMS
+
+def check(inputs)
+	result = inputs.shift
+	results = [inputs.shift]
+	inputs.each do |i|
+		new_r = []
+		results.each do |r|
+			new_r.append(r + i)
+			new_r.append(r * i)
+		end
+		results = new_r
+	end
+	return results.has?(result) ? result : 0
+end
+
+ans = rows.map { |row| check(row) }.sum
+
+# ==============================================================================
+# END
+# ==============================================================================
+puts ans.s.bold.yellow
+puts 1545311493300.s.bold.green
+ans.clipboard unless ans == 0
+#submit(ans)
+
