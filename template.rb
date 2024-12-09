4652728ae33378b9c02bdddc5bc4d5dd660252be
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-12-09 16:44:09 -0600

    template.rb: Updating.

--- template.rb
+++ template.rb
@@ -11,36 +11,46 @@ inputs[2] = <<-EOF
 
 EOF
 
-input = inputs[1]
-input = inputs[2]
-LINES = input.lines
-PARA  = input.lines.split('')
-WORDS = input.lines.map(&:words)
-NUMS  = input.lines.map(&:numbers)
-ABS   = input.lines.map(&:numbers).map(&:abs)
-CHARS = input.lines.mchars
-CSV   = input.lines.map{|line| line.split(',')}
-GRID  = input.lines.mchars
-height, width = GRID.height, GRID.width if GRID.grid?
-puts "#{height}x#{width} grid".bold.blue if GRID.grid?
-input_stats(inputs[1])
-ans = 0
-
 # ==============================================================================
 # CODE
 # ==============================================================================
-rows = LINES
-rows.mapi { |row, r| }
-rows.eachi do |row, r|
+def solve(input)
+
+	lines    = input.lines
+	para     = input.lines.split('')
+	words    = input.lines.map(&:words)
+	nums     = input.lines.map(&:numbers)
+	abs_nums = input.lines.map(&:numbers).map(&:abs)
+	chars    = input.lines.mchars
+	csv      = input.lines.map{|line| line.split(',')}
+	grid     = input.lines.mchars
+
+	# Grid
+	height, width = grid.height, grid.width if grid.grid?
+	puts "#{height}x#{width} grid".bold.blue if grid.grid?
+
+	ans = 0
+
+	rows = lines
+	rows.mapi { |row, r| }
+	rows.eachi do |row, r|
+	end
+	p rows
+	#rows = rows.border(1,'X')
+	#puts rows.mmjoin
+
+	return ans
 end
-p rows
-#rows = rows.border(1,'X')
-#puts rows.mmjoin
-
 
 # ==============================================================================
-# END
+# SUBMIT
 # ==============================================================================
+select = 1
+select = 2
+input = inputs[select]
+input_stats(inputs[1])
+ans = solve(input)
+
 puts ans.s.bold.yellow
 #puts .s.bold.green
 ans.clipboard unless ans == 0
