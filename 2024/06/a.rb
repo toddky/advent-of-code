eb03b32f8806a02c7ba57dae2f194b338614210b
Todd Yamakawa <tyamakawa@tenstorrent.com>
2024-12-17 16:23:49 -0600

    2024/06/a.rb: Cleaning up.

--- 2024/06/a.rb
+++ 2024/06/a.rb
@@ -21,9 +21,7 @@ inputs[2] = <<-EOF
 EOF
 
 input = inputs[0]
-#input = inputs[1]
 #input = inputs[2]
-LINES = input.lines
 GRID  = input.lines.mchars
 height, width = GRID.height, GRID.width if GRID.grid?
 puts "#{height}x#{width} grid".bold.blue if GRID.grid?
@@ -32,37 +30,34 @@ puts "#{height}x#{width} grid".bold.blue if GRID.grid?
 # CODE
 # ==============================================================================
 rows = GRID
-gr = LINES.index { |l| l.include? '^' }
-gc = LINES[gr].chars.index('^')
 
 # Border magic
-#puts rows.mjoin.join("\n")
+#puts rows.mmjoin
 rows = rows.border(1,'X')
-#puts rows.mjoin.join("\n")
-gr += 1
-gc += 1
-#rows[gr][gc] = '.'
+#puts rows.mmjoin
+
+gr,gc = rows.gindex('^').flatten
 gdir = [-1, 0]
 
-next_dir = {
+turn_right = {
 	[-1,0] => [0,1],
 	[0,1] => [1,0],
 	[1,0] => [0,-1],
 	[0,-1] => [-1,0]
 }
 
-pos = []
+pos = Set.new
 while rows[gr][gc] != 'X'
 	pos.append([gc,gr])
 	next_gr = gr + gdir[0]
 	next_gc = gc + gdir[1]
 	if rows[next_gr][next_gc] == '#'
-		gdir = next_dir[gdir]
+		gdir = turn_right[gdir]
 	end
 	gr = gr + gdir[0]
 	gc = gc + gdir[1]
 end
-ans = pos.uniq.len
+ans = pos.len
 
 # ==============================================================================
 # END
