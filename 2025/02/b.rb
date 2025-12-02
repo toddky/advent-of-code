4687eb5252014d9ed7e0b618cba6595e96b0f1c0
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-12-01 23:46:00 -0600

    2025/02: Solved!

---
+++ 2025/02/b.rb
@@ -0,0 +1,72 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+# ==============================================================================
+# SETUP
+# ==============================================================================
+require_relative '../../todd'
+File.delete('answer.txt') if File.exists?('answer.txt')
+
+inputs = Hash.new(nil)
+params = Hash.new(nil)
+answers = Hash.new(nil)
+
+# Real input
+inputs[1] = 'input.txt'.read
+params[1] = nil
+answers[1] = 27469417404
+
+# Example input
+inputs[2] = <<-EOF
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+EOF
+params[2] = nil
+answers[2] = 4174379265
+
+# ==============================================================================
+# CODE
+# ==============================================================================
+def solve(input, param)
+	rows = input.lines.join('').split(',')
+
+	ans = 0
+	rows.each do |row|
+		a, b = row.split('-').i
+		(a..b).each do |n|
+			invalid = (n.s =~ /^(\d+)(\1)+$/)
+			ans += n if invalid
+		end
+	end
+	return ans
+end
+
+
+# ==============================================================================
+# SUBMIT
+# ==============================================================================
+input_stats(inputs[1])
+sel = 1
+sel = 2
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+
+exit unless sel != 1 and ans == exp_ans
+sel = 1
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+
+if not exp_ans.nil?
+	puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+else
+	File.write('answer.txt', ans)
+	puts 'Wrote to answer.txt'
+	ans.clipboard
+	#submit(ans)
+end
+
