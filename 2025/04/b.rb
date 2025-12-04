e3427232dd34ba4730472c31c9dc35903356013d
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-12-03 23:12:58 -0600

    2025/04: Solved!

---
+++ 2025/04/b.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+# ==============================================================================
+# SETUP
+# ==============================================================================
+require_relative '../../todd'
+File.delete('answer.txt') if File.exists?('answer.txt')
+
+inputs = Hash.new(nil)
+params = Hash.new(nil)
+answers = Hash.new(nil)
+
+# Real input
+inputs[1] = 'input.txt'.read
+params[1] = nil
+answers[1] = 9024
+
+# Example input
+inputs[2] = <<-EOF
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+EOF
+params[2] = nil
+answers[2] = 43
+
+
+# ==============================================================================
+# CODE
+# ==============================================================================
+def solve(input, param)
+	rows = input.lines.mchars.border(1,'.')
+
+	ans = 0
+	remove = []
+	while true
+		rows.geach do |r, c, v|
+			next if v == '.'
+			remove.append([r,c]) if (Dir.D9(r,c).map { |r1,c1| rows[r1][c1] }.count('@') <= 4)
+		end
+		ans += remove.len
+		remove.each { |r,c| rows[r][c] = '.' }
+		return ans if remove.empty?
+		remove = []
+	end
+end
+
+
+# ==============================================================================
+# SUBMIT
+# ==============================================================================
+input_stats(inputs[1])
+sel = 1
+sel = 2
+
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+
+exit unless sel != 1 and ans == exp_ans
+sel = 1
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+
+if not exp_ans.nil?
+	puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+else
+	File.write('answer.txt', ans)
+	puts 'Wrote to answer.txt'
+	ans.clipboard
+	#submit(ans)
+end
+
