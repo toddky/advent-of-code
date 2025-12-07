f4f3d4ec9a46a63a7a8ca47568317114c67c0170
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-12-06 23:31:49 -0600

    2025/07: Solved!

---
+++ 2025/07/a.rb
@@ -0,0 +1,105 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+# ==============================================================================
+# SETUP
+# ==============================================================================
+require_relative '../../todd'
+File.delete('answer.txt') if File.exists?('answer.txt')
+
+inputs = Hash.new(nil)
+params = Hash.new(nil)
+answers = Hash.new(nil)
+
+# Real input
+inputs[1] = 'input.txt'.read
+params[1] = nil
+answers[1] = 1594
+
+# Example input
+inputs[2] = <<-EOF
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+EOF
+params[2] = nil
+answers[2] = 21
+
+
+# ==============================================================================
+# CODE
+# ==============================================================================
+def solve(input, param)
+	grid = input.lines.mchars
+
+	ans = 0
+	beams = [[0, grid.first.index('S')]]
+
+	while not beams.empty?
+		r,c = beams.shift
+		r += 1
+		next if r >= grid.len
+
+		if grid[r][c] == '.'
+			grid[r][c] == '|'
+			beams.append([r,c])
+			next
+		end
+
+		next unless grid[r][c] == '^'
+
+		if grid[r][c+1] == '.'
+			grid[r][c+1] = '|'
+			beams.append([r,c+1])
+		end
+
+		if grid[r][c-1] == '.'
+			grid[r][c-1] = '|'
+			beams.append([r,c-1])
+			ans += 1
+		end
+
+	end
+	return ans
+end
+
+
+# ==============================================================================
+# SUBMIT
+# ==============================================================================
+sel = 1
+sel = 2
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+
+exit unless sel != 1 and ans == exp_ans
+sel = 1
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+
+if not exp_ans.nil?
+	puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+else
+	File.write('answer.txt', ans)
+	puts 'Wrote to answer.txt'
+	ans.clipboard
+	#submit(ans)
+end
+
