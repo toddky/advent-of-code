578b53334f460ddac3450bd48494dddb3eea9d36
Todd Yamakawa <tyamakawa@tenstorrent.com>
2025-12-11 22:57:11 -0600

    2025/11/a.rb: Solved!

---
+++ 2025/11/a.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+# vim: ft=ruby noet ts=4 sw=0 sts
+# ==============================================================================
+# SETUP
+# ==============================================================================
+require_relative '../../todd'
+File.delete('answer.txt') if File.exists?('answer.txt')
+
+inputs = Hash.new(nil)
+params = Hash.new(nil)
+answers = Hash.new(nil)
+
+# Real input
+inputs[1] = 'input.txt'.read
+params[1] = nil
+answers[1] = 674
+
+# Example input
+inputs[2] = <<-EOF
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+EOF
+params[2] = nil
+answers[2] = 5
+
+
+# ==============================================================================
+# CODE
+# ==============================================================================
+def solve(input, param)
+	lines = input.lines
+
+	map = {}
+	lines.each do |line|
+		start, nodes = line.split(':')
+		map[start] = nodes.words
+	end
+
+	ans = 0
+	paths = [['you']]
+	while not paths.empty?
+		new_paths = []
+		paths.each do |path|
+			last = path.last
+			if last == 'out'
+				ans += 1
+				next
+			end
+			new_paths += map[last].map { |n| path + [n] }
+		end
+		paths = new_paths
+	end
+	return ans
+end
+
+
+# ==============================================================================
+# SUBMIT
+# ==============================================================================
+sel = 1
+sel = 2
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+
+exit unless sel != 1 and ans == exp_ans
+sel = 1
+ans = solve(inputs[sel], params[sel])
+sel_s = "[#{sel}]".bold.blue
+exp_ans = answers[sel]
+puts "#{sel_s} #{ans.s.bold.yellow}"
+
+if not exp_ans.nil?
+	puts "#{sel_s} #{exp_ans.nil? ? 'UNDEF'.bold.red : exp_ans.s.bold.green}"
+else
+	File.write('answer.txt', ans)
+	puts 'Wrote to answer.txt'
+	ans.clipboard
+	#submit(ans)
+end
+
